94ed622918efde24f1e7c2ae8b8107b90ea4f7f1
Ahmet Şengül <74301219+Ahmet-Sengul@users.noreply.github.com>
2021-01-27 13:22:21 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        static void AddCustomer(Customers customer)
+        {
+            Console.WriteLine("Customer Added!");
+        }
+        static void ListCostumer(Customers customer)
+        {
+            Console.WriteLine("Customer Listed!");
+        }
+        static void DelCustomer(Customers customer)
+        {
+            Console.WriteLine("Customer Deleted!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customers customer1 = new Customers()
+            {
+                Id = 1,
+                FirstName = "Ahmet",
+                LastNAme = "Şengül"
+            };
+            Customers customer2 = new Customers()
+            {
+                Id = 1,
+                FirstName = "Furkan",
+                LastNAme = "Kavak"
+            };
+        }
+    }
+}
